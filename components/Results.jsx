add8c484eb78e148fcdf0ec92ba6d7f8cc928999
Henok <hennok100@gmai.com>
2024-04-12 18:44:29 -0400

    added card component

--- components/Results.jsx
+++ components/Results.jsx
@@ -1,14 +1,11 @@
 import React from "react";
+import Card from "./Card";
 
 const Results = ({ results }) => {
   return (
-    <div>
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
       {results.map((result) => {
-        return (
-          <div key={result.title}>
-            <h1>{result.title}</h1>
-          </div>
-        );
+        return <Card key={result.id} result={result} />;
       })}
     </div>
   );
