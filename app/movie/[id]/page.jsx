9699e27edbe36ff22cc48915722700c3a5177627
Henok <hennok100@gmai.com>
2024-04-12 23:49:49 -0400

    final commit

--- app/movie/[id]/page.jsx
+++ app/movie/[id]/page.jsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
 
-const MoviePage = async ({ params }) => {
+export default async function MoviePage({ params }) {
   const movieId = params.id;
-
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`
   );
-
   const movie = await res.json();
+  // console.log(movie);
 
   return (
     <div className="w-full">
@@ -16,9 +15,9 @@ const MoviePage = async ({ params }) => {
           src={`https://image.tmdb.org/t/p/original/${
             movie.backdrop_path || movie.poster_path
           }`}
-          alt=""
           width={500}
           height={300}
+          alt=""
           className="rounded-lg"
           style={{ maxWidth: "100%", height: "100%" }}
         />
@@ -27,18 +26,16 @@ const MoviePage = async ({ params }) => {
             {movie.title || movie.name}
           </h2>
           <p className="text-lg mb-3">{movie.overview}</p>
-          <p>
+          <p className="mb-3">
             <span className="font-semibold mr-1">Date Released:</span>
             {movie.release_date || movie.first_air_date}
           </p>
-          <p>
-            <span className="font-semibold mr-1">Rating: </span>
+          <p className="mb-3">
+            <span className="font-semibold mr-1">Rating:</span>
             {movie.vote_count}
           </p>
         </div>
       </div>
     </div>
   );
-};
-
-export default MoviePage;
+}
